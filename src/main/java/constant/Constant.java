c9e1d2343ea8fb382d810906ed210c9eec33b5ae
xiaxia112 <33377370+xiaxia112@users.noreply.github.com>
2022-03-09 19:34:28 +0800

    create CodeFileTag & TextFileTag & LogFileTag

--- src/main/java/constant/Constant.java
+++ src/main/java/constant/Constant.java
@@ -1,14 +1,22 @@
 package constant;
 
 import java.util.ResourceBundle;
-
+/**
+ * @author Xia Yingfeng
+ * @date 2022/3/9
+ */
 public class Constant {
     public static final String LIN = "linux";
     public static final String WIN = "windows";
     public static final String ENV_PATH = "config/env";
 
-    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(ENV_PATH);
     public static final String INDEX_PATH, REPOS_DIR, ORIGIN_PROPORTION, PLATFORM;
+    /**
+     * before match, we should toUpperCase() the file name
+     */
+    public static final String[] KEY_WORDS = {"CHANGELOG", "CHANGE LOG", "README", "READ ME"};
+    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(ENV_PATH);
+
     static {
         PLATFORM = RESOURCE_BUNDLE.getString("platform");
         INDEX_PATH = RESOURCE_BUNDLE.getString("index_path");
